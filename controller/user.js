ec2550cbd7e5fad103c1d0e5ae5f02f3a6c5d033
Aoufi Abderahmane <aoufi.abderahmane@lastingdynamics.com>
2022-02-19 17:18:42 +0100

    encrypt password

--- controller/user.js
+++ controller/user.js
@@ -1,14 +1,22 @@
 import User from "../model/user.js";
+import bcrypt from "bcryptjs";
 
 export const createUser = (req, res, next) => {
     const userInfo = req.body;
-    const user = new User(userInfo);
-    user
-        .save()
-        .then(() => {
-            console.log("User created successfuly");
+    console.log(userInfo.password);
+    bcrypt
+        .hash(userInfo.password, 12)
+        .then((hasehPasswrd) => {
+            userInfo.password = hasehPasswrd;
+            const user = new User(userInfo);
+            user
+                .save()
+                .then(() => {
+                    console.log("User created successfuly");
+                })
+                .catch(() => {
+                    console.log("Something went wrong");
+                });
         })
-        .catch(() => {
-            console.log("Something went wrong");
-        });
+        .catch((err) => console.log("ERRR", err));
 };
